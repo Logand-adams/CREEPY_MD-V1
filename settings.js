a45db3595e59d6e6aa1d3d8602d5db1937ea5c8e
Logand-adams <loganaddams07@gmail.com>
2025-05-01 07:37:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
-global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernomer = "237670507356"
+global.ownername = "Logan"
+global.ownernumber = '237670507356'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
@@ -24,9 +24,9 @@ global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti91 = false //auto block +91 
 global.autoswview = true //auto view status/story
-global.antilink = true; // Default: disabled
+global.antilink = false; // Default: disabled
 global.autostatusReact = true; // Default: disabled
-global.reactEmoji = '😂'; // Default reaction emoji
+global.reactEmoji = '🇨🇲'; // Default reaction emoji
 //Thank you for using 𝓒𝓡𝓔𝓔𝓟𝓨_𝓜𝓓-𝓥𝟏
 //suppoet us by following our official channel
 //https://whatsapp.com/channel/0029VacQFw65Ui2gGv0Kwk1r
